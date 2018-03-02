d787d4409c264e4b2216818694f5914f2fc184d4
adam n <adam.nawroc@gmail.com>
2018-03-02 13:44:59 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -12,7 +12,7 @@ def second_challenge
 
       },
       hero_friends: {
-        
+
       }
 
       },
@@ -27,7 +27,7 @@ def second_challenge
       },
 
       heroine: {
-        
+
       },
 
       heroine_friends: {}
