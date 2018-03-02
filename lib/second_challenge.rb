cd0017d64ca549134a1944f4e6a9aca430024cab
adam n <adam.nawroc@gmail.com>
2018-03-02 13:44:57 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -18,6 +18,19 @@ def second_challenge
       },
     :capulet => {
       #your key/value pairs here!
+
+      patriarch: {
+
+      },
+      matriarch: {
+
+      },
+
+      heroine: {
+        
+      },
+
+      heroine_friends: {}
       }}
 
 
